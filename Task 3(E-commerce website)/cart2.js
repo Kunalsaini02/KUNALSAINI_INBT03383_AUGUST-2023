37a318707ee18306b7e727af6df255bbac031541
Kunal Saini <97625244+Kunalsaini02@users.noreply.github.com>
2023-09-12 22:34:41 +0530

    Add files via upload

---
+++ Task 3(E-commerce website)/cart2.js
@@ -0,0 +1,94 @@
+console.log("Here")
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+
+if(bar){
+    bar.addEventListener("click", () => 
+    {
+        nav.classList.add('active');
+
+    })
+    if(close){
+        close.addEventListener("click", () => 
+        {
+            nav.classList.remove ('active');
+    
+        })
+    }
+}
+
+
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else{
+    ready()
+}
+
+function ready() {
+        var removeCartItemButtons = document.getElementsByClassName('far fa-times-circle')
+        console.log(removeCartItemButtons)
+        for (var i = 0; i < removeCartItemButtons.length; i++) {
+            var button = removeCartItemButtons[i]
+            button.addEventListener('click', removeCartItem)
+    }
+
+    var quantityInputs = document.getElementsByClassName('cart-quantity-input')
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i] 
+        input.addEventListener('change', quantityChanged)
+    }
+
+    var addToCartButtons = document.getElementsByClassName('shop-item-button')
+    for (var i = 0; i < addToCartButtons.length; i++) {
+        var button = addToCartButtons[i]
+        button.addEventListener('click', addToCartClicked)
+    }      
+}
+
+function removeCartItem(event) {
+    var buttonClicked = event.target
+            buttonClicked.parentElement.parentElement.remove()
+            updateCartTotal()
+}
+
+function quantityChanged(event) {
+    var input = event.target
+    if (isNaN(input.value) ||  input.value <= 0){
+        input.value = 1
+    }
+    updateCartTotal()
+}
+
+function addToCartClicked(event){
+    var button = event.target
+    var shopItem = button.parentElement.parentElement
+    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
+    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
+    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
+    console.log(title, price, imageSrc)
+    addItemToCart(title, price, imageSrc)
+}
+
+function  addItemToCart(title, price, imageSrc){
+    var cartRow = document.createElement('div')
+    cartRow.innerText = title
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    cartItems.appendChild(cartRow)
+}
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    var total = 0
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        var price = parseFloat(priceElement.innerText.replace('Rs.', ''))
+        var quantity = quantityElement.value
+        total = total + (price * quantity)
+    }
+    total = Math.round(total * 100) / 100
+    document.getElementsByClassName('cart-total-price')[0].innerText = 'Rs.' + total
+}
